3ebe4eb0496212531ff489c74fe3ffbc4d6f30fb
vanessa Carvalho <vnscarvalh@gmail.com>
2026-01-29 01:01:54 -0300

    Simple create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-       private readonly SellerService _sellerService;
+        private readonly SellerService _sellerService;
 
         public SellersController(SellerService sellerService) // Dependency Injection
         {
@@ -15,6 +16,20 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Prevent CSRF attacks 
+
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
