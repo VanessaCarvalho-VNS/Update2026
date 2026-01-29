a76e321199bd5dc8a3dedb8fc7364db013117a59
vanessa Carvalho <vnscarvalh@gmail.com>
2026-01-29 12:39:52 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException : Exception
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+}
