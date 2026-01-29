43908435f78c97c61d648430c729fee102d6f39f
vanessa Carvalho <vnscarvalh@gmail.com>
2026-01-29 11:01:31 -0300

    Foreign Key not null(referential integrity)

---
+++ SalesWebMvc/Migrations/20260129133549_DepartmentForeignKey.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartmentForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmetId",
+                table: "Seller",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartmetId",
+                table: "Seller");
+        }
+    }
+}
